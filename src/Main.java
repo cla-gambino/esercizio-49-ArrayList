34170c844ae9e35c969ec86c84e01cf5fcb717a3
cla-gambino <154328607+cla-gambino@users.noreply.github.com>
2024-02-07 16:52:25 +0100

    students

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Student student1 = new Student("Claudio Gomes", 23);
+        Student student2 = new Student("Matteo Brunori", 29);
+        Student student3 = new Student("Edoardo Soleri", 26);
+        List<Student> student = Arrays.asList(student1, student2, student3);
+
+        System.out.println("lista studenti: " + student);
+
+        List<Student> newStudents = new ArrayList<>(student);
+        newStudents.add(new Student("Jacopo Segre", 26));
+        newStudents.add(new Student("Fabio Lucioni", 36));
+        newStudents.add(new Student("Francesco Di Mariano", 27));
+        newStudents.add(new Student("Leonardo Mancuaso", 31));
+
+        System.out.println("lista studenti aggiornata: " + newStudents);
+
+
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Student {
+public class Student {
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String name;
+
+    public int age;
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
